1c3dc13aefc52795a1a563da72f1af36b521db1c
Kirill <socolov99@gmail.com>
2024-12-13 15:35:50 +0300

    "Моя статистика" теперь отображает только уникальные даты; добавлен новый текст сообщений в "Лидерборд" (на того кто больше всего не пил и на тех кто выпил сегодня)

--- src/app/handlers/user.py
+++ src/app/handlers/user.py
@@ -1,14 +1,11 @@
 from aiogram import Router, F
 from aiogram.types import Message, CallbackQuery
-from aiogram.filters import CommandStart, Command
+from aiogram.filters import CommandStart
 from aiogram.filters.callback_data import CallbackData
-from aiogram.fsm.context import FSMContext
-from aiogram_calendar import DialogCalendar, DialogCalendarCallback, get_user_locale
-from datetime import date
+from aiogram_calendar import DialogCalendar, DialogCalendarCallback
+from datetime import datetime
 import src.app.keyboards.kb as kb
 from src.database.requests import (set_user, get_user, add_user_drink, get_user_drinks, get_drink_board)
-
-DATE_FORMAT = "%d/%m/%Y"
 
 user_router = Router()
 
@@ -27,14 +24,21 @@ async def user_drink(message: Message):
 
 @user_router.callback_query(DialogCalendarCallback.filter())
 async def process_dialog_calendar(callback_query: CallbackQuery, callback_data: CallbackData):
-    selected, date = await DialogCalendar().process_selection(callback_query, callback_data)
-    if selected:
+    is_selected, selected_date = await DialogCalendar().process_selection(callback_query, callback_data)
+    if is_selected:
         user = await get_user(callback_query.from_user.id)
-        if date <= date.today():
-            await add_user_drink(user.id, date)
-            await callback_query.message.answer("Одобряю твой поступок", reply_markup=kb.main)
+        if selected_date <= datetime.now():
+            await add_user_drink(user.id, selected_date)
+            answer_text = (
+                "Сюююююююда!!! Правильно, сегодня отдыхаем" if selected_date.strftime(
+                    '%Y-%m-%d') == datetime.now().strftime('%Y-%m-%d')
+                else "Одобряю твой поступок"
+            )
+            await callback_query.message.answer(answer_text, reply_markup=kb.main)
         else:
-            await callback_query.message.answer("Выбери наступившую дату", reply_markup=kb.main)
+            await callback_query.message.answer(
+                "Я так понимаю ты рассказал мне о своих планах, но все же выбери уже наступившую дату",
+                reply_markup=kb.main)
 
 
 @user_router.message(F.text == "Моя статистика")
@@ -42,11 +46,11 @@ async def mystats_handler(message: Message):
     await set_user(message.from_user.id, message.from_user.username)
     user = await get_user(message.from_user.id)
     drinks = await get_user_drinks(user.id)
-    drinks_list = list(drinks)
-    if len(drinks_list) > 0:
+    drink_unique_days_list = list(set([drink.action_dt.strftime('%Y-%m-%d') for drink in list(drinks)]))
+    if len(drink_unique_days_list) > 0:
         message_text = "Ты пил в эти даты:\n-----------------------------------\n"
-        for drink in drinks_list:
-            message_text += f"{drink.action_dt.strftime('%Y-%m-%d')}\n"
+        for drink_day in drink_unique_days_list:
+            message_text += f"{drink_day}\n"
     else:
         message_text = 'Я не знаю когда ты пил... Пора это исправлять.\nКогда выпьешь, нажми на кнопку "Я выпил"'
     await message.answer(message_text)
@@ -58,10 +62,25 @@ async def stats_handler(message: Message):
     stats = await get_drink_board()
     stats_list = list(stats)
     if len(stats_list) > 0:
+        drunk_today_users_list = []
         message_text = "Лидерборд\n-----------------------------------\nПользователь; Крайний раз пил; Дней трезвости\n-----------------------------------\n"
         for index, stats_element in enumerate(stats_list):
             days = stats_element.sober_time.days
+            if days == 0:
+                drunk_today_users_list.append(stats_element.user_name)
             message_text += f"{index + 1}) {stats_element.user_name};  {stats_element.last_drink.strftime('%Y-%m-%d')};  {days} дней\n"
+        drunk_today_users_qty = len(drunk_today_users_list)
+        message_text += "-----------------------------------"
+        if drunk_today_users_qty > 0:
+            message_suffix_you = "ты"
+            message_suffix_i = ""
+            if drunk_today_users_qty > 1:
+                message_suffix_you = "вы"
+                message_suffix_i = "и"
+            message_text += f"""\n\n{", ".join(drunk_today_users_list)} - {message_suffix_you} сегодня хорош{message_suffix_i}, так держать !!!"""
+        else:
+            message_text += "\nСегодня еще никто не пил ? Займитесь делом !"
+        message_text += f"\n\n{stats_list[-1].user_name} занимается ерундой, пора выпить..."
     else:
         message_text = "Я пока не собрал статистику. Пьем активнее !"
     await message.answer(message_text)

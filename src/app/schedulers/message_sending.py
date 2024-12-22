30a6c16818025c76d3c7639ec095ef77aef0904f
Kirill <socolov99@gmail.com>
2024-12-22 13:09:48 +0300

    Сообщения Антону удалены

--- src/app/schedulers/message_sending.py
+++ src/app/schedulers/message_sending.py
@@ -24,31 +24,7 @@ CHAT_ID = EVENING_CHAT_ID
 
 
 async def morning_message_cron(bot: Bot):
-    message_text = "Всем доброго утра !\nАнтон, членосос, вставай и пиздуй в вонючем вагоне в свой офис !"
-    try:
-        await bot.send_message(CHAT_ID, message_text)
-    except Exception as e:
-        print(f"Ошибка при отправке сообщения в чат: {e}")
-
-
-async def anton_start_working_message_cron(bot: Bot):
-    message_text = "Антон, раб ебаный, я надеюсь ты уже в офисе ?! "
-    try:
-        await bot.send_message(CHAT_ID, message_text)
-    except Exception as e:
-        print(f"Ошибка при отправке сообщения в чат: {e}")
-
-
-async def anton_eating_message_cron(bot: Bot):
-    message_text = "Антох, сходи поешь, тебе еще долго РАБствовать"
-    try:
-        await bot.send_message(CHAT_ID, message_text)
-    except Exception as e:
-        print(f"Ошибка при отправке сообщения в чат: {e}")
-
-
-async def anton_working_message_cron(bot: Bot):
-    message_text = "Антоша, РАБ сраный, работай - солнце еще высоко !"
+    message_text = "Всем доброго утра !"
     try:
         await bot.send_message(CHAT_ID, message_text)
     except Exception as e:
